7127acf4839b5a076efae10ecccc7b96489dfce1
kipchumba-ket <kipchumbaelly97@gmail.com>
2022-11-24 23:57:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const employee = {
-    name: "Optasia",
-    streetAddress: "Drumvale"
-  }
-  
-  function  updateEmployeeWithKeyAndValue(){
-    return {...employee,["name"]: "Sam",["streetAddress"]: "11 Broadway"}
-  }
-   
-  function destructivelyUpdateEmployeeWithKeyAndValue(){
-    employee.name="Sam",
-    employee.streetAddress="12 Broadway"
-    return employee;
-  }
-  
-  
-  function deleteFromEmployeeByKey(employee,key){
-    const newEmployee = {...employee};
-    delete newEmployee.name;
-     return newEmployee;
-  }
-  deleteFromEmployeeByKey();
-  
-  function destructivelyDeleteFromEmployeeByKey(employee, key){
-    delete employee.name
-    return employee;
-  }
+  name:"Juliet",
+  streetAddress:"002-587"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+  const myNewObject = { ...obj };
+  myNewObject[key] = value;
+
+  return myNewObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+  obj[key] = value;
+  return obj;
+}
+
+
+// deletes `key` from a clone of employee and returns the new employee (it is non-destructive)
+// does not modify the original employee (it is non-destructive)
+function deleteFromEmployeeByKey(obj, key){
+
+  // need to do spread operator
+  const myNewObject = { ...obj };
+  delete myNewObject[key]
+
+  return myNewObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  delete employee[key];
+  return employee;
+}
