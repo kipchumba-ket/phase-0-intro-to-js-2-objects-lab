4e7973b81bbf7e3338388d86944ab85dec9c3e69
kipchumba-ket <kipchumbaelly97@gmail.com>
2022-11-30 12:56:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
+// Write your solution in this file!
 const employee = {
-  name:"Juliet",
-  streetAddress:"002-587"
+  name: "Sam",
+  streetAddress: "11 Broadway",
 }
 
-function updateEmployeeWithKeyAndValue(obj, key, value){
-  const myNewObject = { ...obj };
-  myNewObject[key] = value;
-
-  return myNewObject;
+function 
+updateEmployeeWithKeyAndValue(obj, key, value) 
+{
+return Object.assign ({}, obj, {[key]:value})
+}
+function 
+destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) 
+{
+obj[key] = value
+return obj
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
-  obj[key] = value;
-  return obj;
+function 
+deleteFromEmployeeByKey(employee, key) 
+{
+  const newObj = Object.assign ({}, employee, key)
+delete newObj[key]
+return newObj
 }
-
-
-// deletes `key` from a clone of employee and returns the new employee (it is non-destructive)
-// does not modify the original employee (it is non-destructive)
-function deleteFromEmployeeByKey(obj, key){
-
-  // need to do spread operator
-  const myNewObject = { ...obj };
-  delete myNewObject[key]
-
-  return myNewObject;
-}
-
-function destructivelyDeleteFromEmployeeByKey(employee, key){
-  delete employee[key];
-  return employee;
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key]
+  return employee
 }
